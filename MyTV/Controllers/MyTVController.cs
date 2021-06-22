9710abced20fea4a3c39e93ac3fb633ad4327c31
Tan Tano <tantn1@sunshinegroup.vn>
2021-06-22 14:47:52 +0700

    init project

---
+++ MyTV/Controllers/MyTVController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyTV.Controllers
+{
+    public class MyTVController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
